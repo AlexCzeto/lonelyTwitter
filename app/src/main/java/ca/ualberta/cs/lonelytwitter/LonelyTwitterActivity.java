64a151b728d2de5372d7986810e6c4a64bb7070f
AlexCzeto <alexczeto@yahoo.ca>
2017-01-24 19:56:19 -0700

    Real Commit

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -0,0 +1,145 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LonelyTwitterActivity extends Activity {
+
+	private static final String FILENAME = "file.sav";
+	private EditText bodyText;
+	private ListView oldTweetsList;
+	private ArrayList<Tweet> tweetList;
+	private ArrayAdapter<Tweet> adapter;
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bodyText = (EditText) findViewById(R.id.body);
+		Button saveButton = (Button) findViewById(R.id.save);
+		Button clearButton = (Button) findViewById(R.id.clear);
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+
+		//Tweet tweet2 = new Tweet(new Date(),"Second tweet");
+/*		try {
+			Tweet tweet = new NormalTweet("First tweet");
+			tweet.setMessage("HELLO");
+			ImportantTweet importantTweet = new ImportantTweet("very important");
+			NormalTweet normalTweet = new NormalTweet("im normal");
+
+			ArrayList<Tweet> arrayList = new ArrayList<Tweet>();
+			arrayList.add(tweet);
+			arrayList.add(importantTweet);
+			arrayList.add(normalTweet);
+
+		}catch(TweetTooLongException e){
+			e.printStackTrace();
+		}
+*/
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+
+                Tweet tweet = null;
+                try {
+                    tweet = new NormalTweet(text);
+                } catch (TweetTooLongException e) {
+                    throw new RuntimeException();
+                }
+                tweetList.add(tweet);
+
+				adapter.notifyDataSetChanged();
+
+				saveInFile();
+
+			}
+		});
+
+        clearButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                tweetList.clear();
+                adapter.notifyDataSetChanged();
+                deleteFile(FILENAME);
+
+            }
+
+        });
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		loadFromFile();
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
+		oldTweetsList.setAdapter(adapter);
+	}
+
+	private void loadFromFile() {
+		try {
+			FileInputStream fis = openFileInput(FILENAME);
+			BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+            // Taken from http://stackoverflow.com/questions/27014417/how-to-use-gson-to-convert-json-to-arraylist-if-the-list-contain-different-class
+            // 2017-01-24 18:19
+            Type listType = new TypeToken<ArrayList<NormalTweet>>(){}.getType();
+            tweetList = gson.fromJson(in,listType);
+
+		} catch (FileNotFoundException e) {
+            tweetList = new ArrayList<Tweet>();
+		} catch (IOException e) {
+			// TODO FIX
+            throw new RuntimeException();
+		}
+
+	}
+
+	private void saveInFile() {
+		try {
+			FileOutputStream fos = openFileOutput(FILENAME,
+					Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(tweetList,out);
+            out.flush();
+
+			fos.close();
+		} catch (FileNotFoundException e) {
+			// TODO Handle the Exception properly later
+			throw new RuntimeException();
+		} catch (IOException e) {
+            // TODO Handle the Exception properly later
+            throw new RuntimeException();
+		}
+	}
+}
